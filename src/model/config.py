7abcc14918a67b3f898ca09140059f5a9ce8b007
zac <zacchaeuschok2014@gmail.com>
2024-07-14 02:11:24 +0800

    Add session token to runtime

--- src/model/config.py
+++ src/model/config.py
@@ -12,6 +12,7 @@ bedrock_runtime = boto3.client(
     region_name="us-west-2",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
 )
 
 
