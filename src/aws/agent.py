d4490c81453dbfd170c4daaed935f0d92b9e666a
zac <zacchaeuschok2014@gmail.com>
2024-07-14 04:19:16 +0800

    Install unzip

--- src/aws/agent.py
+++ src/aws/agent.py
@@ -25,6 +25,7 @@ def construct_agent():
             using sudo:
             
             curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            apt-get install unzip
             unzip awscliv2.zip
             ./aws/install --install-dir ~/aws-cli --bin-dir ~/bin
             aws --version
